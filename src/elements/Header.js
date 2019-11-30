ed69b6d8b8499007ed4f8c2f8074e431eb7f71ca
Eri Johnson <erijohnsons.fernandes@gmail.com>
2019-11-30 13:19:22 -0300

    Implementar a Exibição das Movimentações

--- src/elements/Header.js
+++ src/elements/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
         <nav className='navbar navbar-light bg-light'>
             <div className='container'>
-                <a className='navbar-brand'>MyMoney</a>
+                <Link className='navbar-brand' to='/'>MyMoney</Link>
             </div>
         </nav>
     )
--- src/pages/Movimentacoes.js
+++ src/pages/Movimentacoes.js
@@ -1,7 +1,39 @@
 import React from 'react'
 
-const Movimentacoes = () => {
-    return <h1>Movimentações</h1>
+import Rest from '../utils/rest'
+
+const baseUrl = 'https://mymoney-devpleno-eri.firebaseio.com/'
+const { useGet } = Rest(baseUrl)
+
+const Movimentacoes = ({ match }) => {
+    const data = useGet(`movimentacoes/${match.params.data}`)
+    return (
+        <div className='container'>
+            <h1>Movimentações</h1>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Descrição</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { data.data &&
+                        Object
+                            .keys(data.data)
+                            .map(movimentacao => {
+                                return (
+                                    <tr>
+                                        <td>{data.data[movimentacao].descricao}</td>
+                                        <td>{data.data[movimentacao].valor}</td>
+                                    </tr>
+                                )
+                            })
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Movimentacoes
