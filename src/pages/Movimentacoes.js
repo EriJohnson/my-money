5eec7ad28928598a3b8e2e39571281e3a6f68b4d
unknown <erijohnsons.fernandes@gmail.com>
2019-12-02 11:50:28 -0300

    Adicionar a Função Salvar e Deletetar Movimentações

--- src/pages/Movimentacoes.js
+++ src/pages/Movimentacoes.js
@@ -1,12 +1,36 @@
 import React from 'react'
-
+import { useState } from 'react'
 import Rest from '../utils/rest'
 
 const baseUrl = 'https://mymoney-devpleno-eri.firebaseio.com/'
-const { useGet } = Rest(baseUrl)
+const { useGet, usePost, useDelete } = Rest(baseUrl)
 
 const Movimentacoes = ({ match }) => {
     const data = useGet(`movimentacoes/${match.params.data}`)
+    const [postData, salvar] = usePost(`movimentacoes/${match.params.data}`)
+    const [removeData, remover] = useDelete()
+    const [descricao, setDescricao] = useState('')
+    const [valor, setValor] = useState(0.0)
+
+    const onChangeDescricao = evt => {
+        setDescricao(evt.target.value)
+    }
+    const onChangeValor = evt => {
+        setValor(parseFloat(evt.target.value))
+    }
+    const salvarMovimentacao = async() => {
+        await salvar({
+            descricao,
+            valor
+        })
+        setDescricao('')
+        setValor(0)
+        data.refetch()
+    }
+    const removerMovimentacao = async(id) => {
+        await remover(`movimentacoes/${match.params.data}/${id}`)
+        data.refetch()
+    }
     return (
         <div className='container'>
             <h1>Movimentações</h1>
@@ -18,18 +42,29 @@ const Movimentacoes = ({ match }) => {
                     </tr>
                 </thead>
                 <tbody>
-                    { data.data &&
+                    {data.data &&
                         Object
                             .keys(data.data)
                             .map(movimentacao => {
                                 return (
                                     <tr>
                                         <td>{data.data[movimentacao].descricao}</td>
-                                        <td>{data.data[movimentacao].valor}</td>
+                                        <td>
+                                            {data.data[movimentacao].valor}
+                                            <button onClick={() => removerMovimentacao(movimentacao)}>-</button>
+                                        </td>
                                     </tr>
                                 )
                             })
                     }
+                    <tr>
+                        <td>
+                            <input type='text' value={descricao} onChange={onChangeDescricao} /></td>
+                        <td>
+                            <input type='text' value={valor} onChange={onChangeValor} />
+                            <button onClick={salvarMovimentacao}>+</button>
+                        </td>
+                    </tr>
                 </tbody>
             </table>
         </div>
--- src/utils/rest.js
+++ src/utils/rest.js
@@ -26,44 +26,43 @@ const reducer = (state, action) => {
 const init = baseUrl => {
     const useGet = resource => {
         const [data, dispatch] = useReducer(reducer, INITIAL_STATE)
-        useEffect(() => {
+        const carregar = async () => {
             dispatch({ type: 'REQUEST' })
-            axios
-                .get(baseUrl + resource + '.json')
-                .then(res => {
-                    dispatch({ type: 'SUCCESS', data: res.data })
-                })
-        }, [])
-        return data
+            const res = await axios.get(baseUrl + resource + '.json')
+            dispatch({ type: 'SUCCESS', data: res.data })
+
+        }
+        useEffect(() => {
+            carregar()
+        }, [resource])
+        return {
+            ...data,
+            refetch: carregar
+        }
     }
     const usePost = resource => {
         const [data, dispatch] = useReducer(reducer, INITIAL_STATE)
-        const post = data => {
+        const post = async (data) => {
             dispatch({ type: 'REQUEST' })
-            axios
-                .post(baseUrl + resource + '.json', data)
-                .then(res => {
-                    dispatch({
-                        type: 'SUCCESS',
-                        data: res.data
-                    })
-                })
+            const res = await axios.post(baseUrl + resource + '.json', data)
+            dispatch({
+                type: 'SUCCESS',
+                data: res.data
+            })
         }
         return [data, post]
     }
     const useDelete = () => {
         const [data, dispatch] = useReducer(reducer, INITIAL_STATE)
-        const remove = resource => {
+        const remove = async (resource) => {
             dispatch({ type: 'REQUEST' })
-            axios
-                .delete(baseUrl + resource + '.json')
-                .then(() => {
-                    dispatch({
-                        type: 'SUCCESS',
-                    })
-                })
+            await axios.delete(baseUrl + resource + '.json')
+            dispatch({
+                type: 'SUCCESS',
+            })
         }
         return [data, remove]
+        
     }
     return {
         useGet,
